c86c44a33fa0148959f2c8f8f135538d4100dc75
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-11 10:17:16 +0530

    dfkhsdgj

--- src/main/java/com/zosh/model/User.java
+++ src/main/java/com/zosh/model/User.java
@@ -2,6 +2,9 @@ package com.zosh.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,5 +14,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String password;
+
+    private USER_ROLE role;
 }
