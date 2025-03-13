5cb12aada8dd9d29a4b6b226e4375ebb22a071e1
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-13 15:28:42 +0530

    fhdg

--- src/main/java/com/zosh/dto/RestaurantDto.java
+++ src/main/java/com/zosh/dto/RestaurantDto.java
@@ -1,12 +1,28 @@
 package com.zosh.dto;
 
 
+import jakarta.persistence.Column;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class RestaurantDto {
 
     private String title;
+
+    @Column(length=1000)
+    private List<String> images;
+
+    private String description;
+
+
+
+
+
+
+
+
 
 
 
