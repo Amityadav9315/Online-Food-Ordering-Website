8719229ed97a91565b9d2b963bedc8f753b56783
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-13 15:32:41 +0530

    gfhrt

--- src/main/java/com/zosh/OnlineFoodOrderingApplication.java
+++ src/main/java/com/zosh/OnlineFoodOrderingApplication.java
@@ -2,12 +2,17 @@ package com.zosh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
 @SpringBootApplication
 public class OnlineFoodOrderingApplication {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineFoodOrderingApplication.class, args);
+
+        System.out.println();
 	}
+
 
 }
