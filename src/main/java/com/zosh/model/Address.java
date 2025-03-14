e84e6b8935ec0c5cbe74761607d6c03a0734da2c
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-15 00:51:51 +0530

    flksdvjkds

---
+++ src/main/java/com/zosh/model/Address.java
@@ -0,0 +1,18 @@
+package com.zosh.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.tool.schema.spi.SchemaTruncator;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+}
