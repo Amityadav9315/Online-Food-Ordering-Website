74c6a3fa5007f469f99d5b7549eb15f7e48390a8
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-15 01:02:01 +0530

    sdblkfs

--- src/main/java/com/zosh/model/Restaurant.java
+++ src/main/java/com/zosh/model/Restaurant.java
@@ -1,11 +1,13 @@
 package com.zosh.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,5 +37,19 @@ public class Restaurant {
 
     private  String openingHours;
 
+
+    @OneToMany(mappedBy = "restaurant",cascade = CascadeType.ALL,orphanRemoval = true)
     private List<Order> orders=new ArrayList<>();
+
+    @ElementCollection
+    @Column(length = 1000)
+    private List<String>  images;
+
+    private LocalDateTime  registrationDate;
+
+    private boolean open;
+
+    @JsonIgnore
+    @OneToOne(mappedBy = "restaurant",cascade = CascadeType.ALL)
+    private  List<Food> foods=new ArrayList<>();
 }
